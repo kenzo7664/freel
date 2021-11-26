af107591edf3179dc5610b41393f6152cd9d7f83
kenzo7664 <olaotanemmanuel1@gmail.com>
2021-11-26 09:51:48 +0100

    profile image changed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         </div>
         <div className='flex flex-row m-1'>
           <FaTh className='m-1' />
-          <img src={circle} alt='' className='w-5 sm:h-5 ms:h-5 rounded-full m-0.5' />
+          <img src={circle} alt='' className='w-5 sm:h-5 ms:h-5 md:h-5  rounded-full m-0.5' />
         </div>
       </div>
       <Dashboard />
